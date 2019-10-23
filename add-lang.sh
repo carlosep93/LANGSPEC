6710e6f26982f3ce7d6bc29b81f49cd9350f8266
carlosep93 <carlos.e.p93@gmail.com>
2019-10-23 17:03:34 +0200

    bash scripts

--- add-lang.sh
+++ add-lang.sh
@@ -5,11 +5,12 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=30G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=add-lang-fren500k.log
+#SBATCH --output=add-lang-big3-ruen-ft.log
 
 
 
-stdbuf -i0 -e0 -o0 python add_lang.py  data-bin/wmt13.tokenized.32k.500k.fr-en/ \
- --arch multilingual_transformer_iwslt_de_en --save-dir checkpoints/add-lang-fren500k --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000 --lr 0.001 --min-lr 1e-09 --dropout 0.1 --weight-decay 0.0 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 2500 --update-freq 16  --save-interval-updates 5000  --task multilingual_translation   --key en-en --newkey fr-en --reuse decoder --prev-model   checkpoints/esen-interlingua-finetuning --reset-optimizer --lang-pairs fr-en
+stdbuf -i0 -e0 -o0 python add_lang.py  data-bin/europarl/ \
+ --arch multilingual_transformer_big --save-dir checkpoints/add-lang-big3-ruen-ft --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 --lr-scheduler reduce_lr_on_plateau --lr 0.001 --min-lr 1e-09 --dropout 0.3 --weight-decay 0.0 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 2500 --update-freq 16  --save-interval-updates 30000  --task multilingual_translation   --key de-en --newkey ru-en --reuse decoder --prev-model   checkpoints/europarl-big3 --reset-optimizer --lang-pairs ru-en --restore-file checkpoint_best.pt --finetune True
+
 
 
--- train-europarl.sh
+++ train-europarl.sh
@@ -3,12 +3,13 @@
 
 #SBATCH -p veu # Partition to submit to
 #SBATCH --gres=gpu:1
-#SBATCH --mem=30G # Memory
+#SBATCH --mem=50G # Memory
 #SBATCH --ignore-pbs                                                            
-#SBATCH --output=train-europarl-nograd.log
+#SBATCH --output=train-europarl-big-fr-all.log
 
 
 
 stdbuf -i0 -e0 -o0 python train.py  data-bin/europarl/ \
- --arch interlingua_transformer --save-dir checkpoints/europarl-nograd --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000 --lr 0.001 --min-lr 1e-09 --dropout 0.1 --weight-decay 0.0 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 3584 --update-freq 16  --save-interval-updates 30000  --task interlingua_nodistance_translation   --lang-pairs en-de,fr-de,es-de,de-en,fr-en,es-en,en-fr,es-fr,de-fr,de-es,en-es,fr-es --freeze-schedule n-f,n-n,f-n,f-n,n-f,n-n,f-n,n-f,n-n,n-f,n-n,f-n 
+ --arch interlingua_transformer_big --save-dir checkpoints/europarl-big-fr-all --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000 --lr 0.001 --min-lr 1e-09 --dropout 0.3 --weight-decay 0.0 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 2000 --update-freq 16  --save-interval-updates 30000  --task interlingua_nodistance_translation   --lang-pairs de-en,de-es,de-fr,en-de,en-es,en-fr,es-de,es-en,es-fr,fr-de,fr-en,fr-es  --freeze-schedule n-n,f-n,n-f,n-n,n-f,f-n,n-f,f-n,n-n,f-n,n-f,n-n
+
 
